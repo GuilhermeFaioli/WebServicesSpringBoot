7a3b675e9f771a7cff220262dfe0588c0fc5c2e2
GuilhermeFaioli <guilhermefa12@gmail.com>
2021-07-23 17:00:43 -0300

    Exception handling - findById

---
+++ src/main/java/com/guilhermeFaioli/WebServicesSpringBoot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.guilhermeFaioli.WebServicesSpringBoot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource Not found. Id " + id);
+	}
+}
